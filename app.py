d63ef2e2780c63a743f3f1f2f8df2c7ef3a3d96a
PatcharaKongaum <tonnum15@gmail.com>
2025-07-25 12:26:00 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+"""Calculator"""
+
+def main():
+    """Main function"""
+    print("Welcome to calculator")
+    first = int(input())
+    second = int(input())
+    print(f"{first} + {second} =")
+    print("Hello World")
+
+main()
